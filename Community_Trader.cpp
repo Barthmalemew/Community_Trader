d072c6f72b0f08aedd20fd03877dcb5da2808370
Barthmalemew <78800375+Barthmalemew@users.noreply.github.com>
2023-10-28 18:32:55 -0400

    Add files via upload

--- Community_Trader.cpp
+++ Community_Trader.cpp
@@ -3,38 +3,47 @@
 
 using namespace std;
 
+const int MAXSIZE = 5;
+const int NAMESIZE = 5;
+
 bool requestCheck(const bool array[], int INDEX);
 
 void userInput(bool array[], int INDEX);
 
+
 int main()
 {
-    int size = 5;
+    bool requestArray[MAXSIZE];
+    char nameBox[MAXSIZE][NAMESIZE];
 
-    bool requestArray[size];
-
-    for(int i = 0; i < size; i++ )
+    for(int i = 0; i < MAXSIZE; i++ ) 
     {
+        cout << "What is the user's name: ";
+        cin.getline(nameBox[i], NAMESIZE);
+        cin.ignore(numeric_limits<streamsize>::max(), '\n'); 
         userInput(requestArray, i);
+        
     }
 
-    for(int i = 0; i < size; i++ )
+
+    for(int i = 0; i < MAXSIZE; i++ )
     {
         cout << requestCheck(requestArray, i) << endl;
     }
 }
+
 
 bool requestCheck(const bool array[], int INDEX)
 {
     return array[INDEX];    
 }
 
-//this function is going to ask the user if they want to make a request, see if they are avaliable to make a request(as in they have not already made one) let them make a request if they are avaliable, get their request and then change their request status
+
 void userInput(bool array[], int INDEX)
 {
     char decision;
     
-    cout << "User do you want to make a request? Enter y/n: " << "\n";
+    cout << "User do you want to make a request? Enter y/n: ";
     cin >> decision;
 
     decision = tolower(decision);
@@ -48,3 +57,5 @@ void userInput(bool array[], int INDEX)
         array[INDEX] = false;
     }
 }
+
+
