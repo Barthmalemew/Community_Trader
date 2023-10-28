3d02dc8406ae08c2d15a4cdd466561a2d4af6235
Barthmalemew <78800375+Barthmalemew@users.noreply.github.com>
2023-10-28 16:10:08 -0400

    Add files via upload

--- Community_Trader.cpp
+++ Community_Trader.cpp
@@ -2,10 +2,27 @@
 
 using namespace std;
 
+bool requestCheck(const bool array[], int INDEX);
+
 int main()
 {
-    
+    int size = 25;
 
+    bool requestArray[size];
+
+    for(int i = 0; i < size; i++ )
+    {
+        requestArray[i] = 1;
+    }
+
+    for(int i = 0; i < size; i++ )
+    {
+        cout << requestCheck(requestArray, i) << endl;
+    }
 }
 
+bool requestCheck(const bool array[], int INDEX)
+{
+    return array[INDEX];    
+}
 
