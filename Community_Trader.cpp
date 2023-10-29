c6861cd96b6ba71df1c0b0278cdac7f3c603f778
NathanielR2 <143201093+NathanielR2@users.noreply.github.com>
2023-10-29 03:36:07 -0400

    Add files via upload

--- Community_Trader.cpp
+++ Community_Trader.cpp
@@ -21,146 +21,134 @@ void randInnit(bool arrayS[], int MAXSIZE);
 
 int main()
 {
-    int ranStore[MAXSIZE-1];
-    bool requestArray[MAXSIZE];
-    char nameBox[MAXSIZE][NAMESIZE];
-    char requestBox[MAXSIZE][REQUESTSIZE];
+	bool requestArray[MAXSIZE];
+	char nameBox[MAXSIZE][NAMESIZE];
+	char requestBox[MAXSIZE][REQUESTSIZE];
 
-    srand(time(0));
+	srand(time(0));
 
-    int userId = 7;
-    int userIdInput = 0;
-    int pin = 4963;
-    int pinInput = 0;
+	int userId = 7;
+	int userIdInput = 0;
+	int pin = 4963;
+	int pinInput = 0;
 
-    //intitalizing every index with values sans the users index // We have to shorten this 
-    /*
-    requestArray[0] = true;
-    requestArray[1] = true;
-    requestArray[2] = true;
-    requestArray[3] = false;
-    requestArray[4] = false;
-    requestArray[5] = true;
-    requestArray[6] = false;
-    requestArray[7] = false;
-    requestArray[8] = true;
-    requestArray[9] = false;
-    */
-    randInnit(requestArray, MAXSIZE);
+	//intitalizing every index with values sans the users index // We have to shorten this 
+   
+	randInnit(requestArray, MAXSIZE);
 
-    snprintf(nameBox[0], NAMESIZE, "John");
-    snprintf(nameBox[1], NAMESIZE, "Alex");
-    snprintf(nameBox[2], NAMESIZE, "Zoe");
-    snprintf(nameBox[3], NAMESIZE, "Bob");
-    snprintf(nameBox[4], NAMESIZE, "Cindy");
-    snprintf(nameBox[5], NAMESIZE, "Sera");
-    snprintf(nameBox[6], NAMESIZE, "Tom");
-    snprintf(nameBox[8], NAMESIZE, "Kat");
-    snprintf(nameBox[9], NAMESIZE, "Belle");
+	snprintf(nameBox[0], NAMESIZE, "John");
+	snprintf(nameBox[1], NAMESIZE, "Alex");
+	snprintf(nameBox[2], NAMESIZE, "Zoe");
+	snprintf(nameBox[3], NAMESIZE, "Bob");
+	snprintf(nameBox[4], NAMESIZE, "Cindy");
+	snprintf(nameBox[5], NAMESIZE, "Sera");
+	snprintf(nameBox[6], NAMESIZE, "Tom");
+	snprintf(nameBox[8], NAMESIZE, "Kat");
+	snprintf(nameBox[9], NAMESIZE, "Belle");
 
-    snprintf(requestBox[0], REQUESTSIZE, "1 Burger");
-    snprintf(requestBox[1], REQUESTSIZE, "1 Laptop");
-    snprintf(requestBox[2], REQUESTSIZE, "20 Hairbrushs");
-    snprintf(requestBox[3], REQUESTSIZE, "12 Bricks");
-    snprintf(requestBox[4], REQUESTSIZE, "10 Books");
-    snprintf(requestBox[5], REQUESTSIZE, "7 Cats");
-    snprintf(requestBox[6], REQUESTSIZE, "19 Fish");
-    snprintf(requestBox[8], REQUESTSIZE, "11 Knives");
-    snprintf(requestBox[9], REQUESTSIZE, "5 Golden Bells");
+	snprintf(requestBox[0], REQUESTSIZE, "%d Burger[s]",(rand() % 50 +1));
+	snprintf(requestBox[1], REQUESTSIZE, "%d Laptop[s]",(rand() % 50 + 1));
+	snprintf(requestBox[2], REQUESTSIZE, "%d Hairbrushs", (rand() % 50 + 1));
+	snprintf(requestBox[3], REQUESTSIZE, "%d Bricks",(rand() % 50 + 1));
+	snprintf(requestBox[4], REQUESTSIZE, "%d Books", (rand() % 50 + 1));
+	snprintf(requestBox[5], REQUESTSIZE, "%d Cats",(rand() % 50 +1));
+	snprintf(requestBox[6], REQUESTSIZE, "%d Fish", (rand() % 50 + 1));
+	snprintf(requestBox[8], REQUESTSIZE, "%d Knives", (rand() % 50 + 1));
+	snprintf(requestBox[9], REQUESTSIZE, "%d Golden Bells", (rand() % 50 + 1));
 
  ;
 
-    cout << "Enter User ID: ";
-    cin >> userIdInput;
-    cin.ignore(256, '\n');
-    if (userIdInput == userId)
-    {
-        cout << "Enter Passcode: ";
-        cin >> pinInput;
-        cin.ignore(256, '\n');
-        if (pinInput == pin)
-        {
-            printf("Welcome User %d \n", userId);
-        }
-    }
-                  
+	cout << "Enter User ID: ";
+	cin >> userIdInput;
+	cin.ignore(256, '\n');
+	if (userIdInput == userId)
+	{
+		cout << "Enter Passcode: ";
+		cin >> pinInput;
+		cin.ignore(256, '\n');
+		if (pinInput == pin)
+		{
+			printf("Welcome User %d \n", userId);
+		}
+	}
+				  
 
 
 
-    userInput(requestArray, nameBox, requestBox, userId);
+	userInput(requestArray, nameBox, requestBox, userId);
 
-    for(int i = 0; i < MAXSIZE; i++ )
-    {
-       userOutput(requestArray, nameBox, requestBox, i, userId);
-    }
-    userGive(requestArray, nameBox, requestBox, userId);
+	for(int i = 0; i < MAXSIZE; i++ )
+	{
+	   userOutput(requestArray, nameBox, requestBox, i, userId);
+	}
+	userGive(requestArray, nameBox, requestBox, userId);
 
 }
 
 void userInput(bool arrayS[],char arrayN[][NAMESIZE],char arrayR[][REQUESTSIZE], int INDEX)
 {
-    char decision;
-    bool reqCheck = false;
+	char decision;
+	bool reqCheck = false;
 
-    cout << "Enter your name: ";
-    cin.getline(arrayN[INDEX], NAMESIZE);
+	cout << "Enter your name: ";
+	cin.getline(arrayN[INDEX], NAMESIZE);
 
-    cout << "User do you want to make a request? Enter y/n: ";
-    decision = cin.get();
-    cin.ignore(256, '\n' );
-    decision = tolower(decision);
-    
+	cout << "User do you want to make a request? Enter y/n: ";
+	decision = cin.get();
+	cin.ignore(256, '\n' );
+	decision = tolower(decision);
+	
    
-    if(decision == 'y')
-    {
-        reqCheck = true;
-    }
-    else if(decision == 'n')
-    {
-        reqCheck = false;
-    }
-    
-    
-    // checks if user has already made a request and if they are currently making a request
-    if(arrayS[INDEX] == false && reqCheck == true)
-    {
-       
-       cout << "What request do you want to make?: ";
-       cin.getline(arrayR[INDEX], REQUESTSIZE);
-       arrayS[INDEX] = reqCheck; 
-    }
-    else if(arrayS[INDEX] == true && reqCheck == true)
-    {
-        cout << "User already has one pending request." << endl;
-    }
-    else
-    {
-        cout << "Have a good day!" << endl; 
-    }
+	if(decision == 'y')
+	{
+		reqCheck = true;
+	}
+	else if(decision == 'n')
+	{
+		reqCheck = false;
+	}
+	
+	
+	// checks if user has already made a request and if they are currently making a request
+	if(arrayS[INDEX] == false && reqCheck == true)
+	{
+	   
+	   cout << "What request do you want to make?: ";
+	   cin.getline(arrayR[INDEX], REQUESTSIZE);
+	   arrayS[INDEX] = reqCheck; 
+	}
+	else if(arrayS[INDEX] == true && reqCheck == true)
+	{
+		cout << "User already has one pending request." << endl;
+	}
+	else
+	{
+		cout << "Have a good day!" << endl; 
+	}
 }
 
 
 void userOutput(const bool arrayS[],const char arrayN[][NAMESIZE],const char arrayR[][REQUESTSIZE], int INDEX, int uId)
 {
-    if(arrayS[INDEX] == true && INDEX != uId)
-    {
-        cout << INDEX << " ";
-        cout << arrayN[INDEX] << " ";
-        cout << arrayR[INDEX] << endl;
-    }
+	if(arrayS[INDEX] == true && INDEX != uId)
+	{
+		cout << INDEX << " ";
+		cout << arrayN[INDEX] << " ";
+		cout << arrayR[INDEX] << endl;
+	}
 }
 
 void userGive(bool arrayS[], const char arrayN[][NAMESIZE], const char arrayR[][REQUESTSIZE], int uId)
 {
-    
-    
-    char decision = ' ';
-    int targetIndex = 0;
-    int rId = 0; //index of the person the user is fulfilling the request of
+	char decision;
+	int targetIndex;
+	int rId = 0; //index of the person the user is fulfilling the request of
    
-    do
-    {
-        bool reciveGive = false;
+	do
+	{
+		decision = ' ';
+		targetIndex = 0;
+		bool reciveGive = false;
 		cout << "Enter an index of a person whose request you want to fulfill or -1 to exit the program: "; //remeber to tell the user the exit value 
 		cin >> targetIndex;
 
@@ -195,10 +183,10 @@ void userGive(bool arrayS[], const char arrayN[][NAMESIZE], const char arrayR[][
 		{
 			cout << "Invalid index, please try again.\n";
 		}
-    } while (targetIndex != -1);
+	} while (targetIndex != -1);
 
 
-    
+	
 
 
 }
@@ -206,9 +194,9 @@ void userGive(bool arrayS[], const char arrayN[][NAMESIZE], const char arrayR[][
 void randInnit(bool arrayS[], int MAXSIZE)
 {
 
-    for (int i = 0; i < MAXSIZE; i++)
-    {
-        arrayS[i] = bool(rand() > (RAND_MAX / 2));
-    }
+	for (int i = 0; i < MAXSIZE; i++)
+	{
+		arrayS[i] = bool(rand() > (RAND_MAX / 2));
+	}
 }
 
