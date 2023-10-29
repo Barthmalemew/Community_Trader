f69c5c5a08c3f1d4645199c84d99a0bf1b91b441
Barthmalemew <78800375+Barthmalemew@users.noreply.github.com>
2023-10-28 22:03:53 -0400

    Add files via upload

--- Community_Trader.cpp
+++ Community_Trader.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <cctype>
-#include <stdio.h>
 
 using namespace std;
 
@@ -9,24 +8,21 @@ const int NAMESIZE = 5;
 const int REQUESTSIZE = 25;
 
 
-void userInput(const bool array1[],char array2[][REQUESTSIZE], int INDEX);
+void userInput(bool arrayS[],char arrayR[][REQUESTSIZE], int INDEX);
 
-void userOutput(const bool array1[],const char array2[][NAMESIZE],const char array3[][REQUESTSIZE], int INDEX);
-
+void userOutput(const bool arrayS[],const char arrayN[][NAMESIZE],const char arrayR[][REQUESTSIZE], int INDEX);
 
 int main()
 {
     bool requestArray[MAXSIZE] = {1, 0, 1, 1, 0};
     char nameBox[MAXSIZE][NAMESIZE];
-    char requestBox[MAXSIZE][REQUESTSIZE];
+    char requestBox[MAXSIZE][REQUESTSIZE] = {{1, 2, 3, 4, 5}, {}};
+    
+    snprintf(nameBox[0], NAMESIZE, "John");
+    snprintf(nameBox[0], NAMESIZE, "John");
 
-    for(int i = 0; i < MAXSIZE; i++ ) 
-    {
-        cout << "What is the user's name: ";
-        cin.getline(nameBox[i], NAMESIZE, '\n');
-        userInput(requestArray, requestBox, i);
-                
-    }
+    cout << "Enter User ID: ";
+    cout << "Enter Passcode: ";                
 
 
     for(int i = 0; i < MAXSIZE; i++ )
@@ -35,7 +31,7 @@ int main()
     }
 }
 
-void userInput(const bool array1[],char array2[][REQUESTSIZE], int INDEX)
+void userInput(bool arrayS[],char arrayR[][REQUESTSIZE], int INDEX)
 {
     char decision;
     bool reqCheck = false;
@@ -55,16 +51,16 @@ void userInput(const bool array1[],char array2[][REQUESTSIZE], int INDEX)
         reqCheck = false;
     }
     
-    snprintf(array2[INDEX],REQUESTSIZE, "L");
+    
     // checks if user has already made a request and if they are currently making a request
-    if(array1[INDEX] == false && reqCheck == true)
+    if(arrayS[INDEX] == false && reqCheck == true)
     {
        
        cout << "What request do you want to make?:";
-       cin.getline(array2[INDEX], REQUESTSIZE);
-         
+       cin.getline(arrayR[INDEX], REQUESTSIZE);
+       arrayS[INDEX] = reqCheck; 
     }
-    else if(array1[INDEX] == true && reqCheck == true)
+    else if(arrayS[INDEX] == true && reqCheck == true)
     {
         cout << "User already has one pending request." << endl;
     }
@@ -75,12 +71,13 @@ void userInput(const bool array1[],char array2[][REQUESTSIZE], int INDEX)
 }
 
 
-void userOutput(const bool array1[],const char array2[][NAMESIZE],const char array3[][REQUESTSIZE], int INDEX)
+void userOutput(const bool arrayS[],const char arrayN[][NAMESIZE],const char arrayR[][REQUESTSIZE], int INDEX)
 {
-    if(array1[INDEX] == true)
+    if(arrayS[INDEX] == true)
     {
-        cout << array2[INDEX] << endl;
-        cout << array3[INDEX] << endl;
+        cout << INDEX << " ";
+        cout << arrayN[INDEX] << " ";
+        cout << arrayR[INDEX] << endl;
     }
 }
 
